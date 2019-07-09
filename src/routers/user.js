9dfdeadc078644a0746621c1196e81440db9a499
shubhampal98 <shubhampal1898@gmail.com>
2019-07-09 17:17:51 +0530

    sending email not working

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,7 +3,7 @@ const User = require("../models/user");
 const auth = require("../middleware/auth");
 const multer = require("multer");
 const sharp = require("sharp");
-const { sendWelcomeMail, sendCancelEmail } = require("../emails/account");
+//const { sendWelcomeMail, sendCancelEmail } = require("../emails/account");
 const router = new express.Router();
 
 router.post("/users", async (req, res) => {
@@ -11,7 +11,7 @@ router.post("/users", async (req, res) => {
 
   try {
     await user.save();
-    sendWelcomeMail(user.email, user.name);
+    //sendWelcomeMail(user.email, user.name);
     const token = await user.generateAuthToken();
     res.status(201).send({ user, token });
   } catch (e) {
@@ -115,7 +115,7 @@ router.patch("/users/me", auth, async (req, res) => {
 router.delete("/users/me", auth, async (req, res) => {
   try {
     await req.user.remove();
-    sendCancelEmail(req.user.email, req.user.name);
+    //sendCancelEmail(req.user.email, req.user.name);
     res.send(req.user);
   } catch (e) {
     res.status(500).send();
